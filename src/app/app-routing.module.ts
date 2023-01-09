9bf53c9b354eac7d68f91afc9a82e57e137e8652
Hansssbro <josu11042005@gmail.com>
2023-01-09 03:10:37 -0500

    commit8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GymsharkComponent } from './gymshark/gymshark.component';
+import { LoginComponent } from './login/login.component';
 import { OlympusComponent } from './olympus/olympus.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { RawComponent } from './raw/raw.component';
 import { YounglaComponent } from './youngla/youngla.component';
 
 const routes: Routes = [
+{
+  path:'',component: LoginComponent
+},
 {
   path:'pr',component: PrincipalComponent
 },
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -17,7 +17,7 @@ export class PrincipalComponent implements OnInit {
     this.router.navigate(['olp'])
   }
 
-  googs(){
+  gogs(){
     this.router.navigate(['gs'])
   }
   
